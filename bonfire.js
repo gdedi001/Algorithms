f26c4443926e2595b1ad75f773edd3f1adea9d49
GabrielD <gdedi651@gmail.com>
2016-04-23 20:37:33 -0400

    add Advanced Inventory algorithm :)

--- bonfire.js
+++ bonfire.js
@@ -1178,5 +1178,68 @@ function add() {
 
 add(2)(3);
 
+/*
+  Inventory Update
+  
+Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. 
+Update the current existing inventory item quantities (in arr1). 
+If an item cannot be found, add the new item and quantity into the inventory array. 
+The returned inventory array should be in alphabetical order by item.
+
+*/
+function updateInventory(curInv, newInv) {
+    var updated = curInv;
+    var amount = 0;
+    var itemName = 1;
+    
+    for (var i = 0; i < newInv.length; i++){
+        for (var x = 0; x < curInv.length; x++){
+            // check if the new item exists in current inv
+            if (newInv[i][itemName] === curInv[x][itemName]) {
+                console.log(curInv[x] + " | " + newInv[i])
+                // update our current inventory with new incoming amount
+                curInv[x][amount] += newInv[i][amount];
+                break;
+            }
+            // if unable to match items push this item onto updated
+            if (x === curInv.length-1) {
+                updated.push(newInv[i]);
+                break;
+            }
+        }
+
+        // if the store is empty, push all new items into inventory
+        if (curInv.length === 0) {
+            updated.push(newInv[i]);
+        }
+    }
+
+    // sort the list in alphabetical order
+    updated.sort(function(a, b){
+        if(a[1] < b[1]) return -1;
+        if(a[1] > b[1]) return 1;
+        return 0;
+    });
+
+    return updated;
+
+}
+
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
 
 /* MORE ALGORITHMS COMING SOON! */
